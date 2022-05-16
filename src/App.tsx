e637819be38975221308cf5352bb75e1ad8c9153
Jay Young <jyoungta@cisco.com>
2022-05-16 12:52:45 -0400

    loading ui framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "./public/css/cui-standard.min.css";
+const forge = require("node-forge");
 
 function App() {
+  const a = 100;
+  const b = forge;
+  const c = 200;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload - bump.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <div className="container">
+          <div className="header-panels">
+            <div className="header-panel">
+              <a
+                className="header__logo"
+                href="http://www.cisco.com"
+                target="_blank"
+              >
+                <span className="icon-cisco"></span>
+              </a>
+              <div className="header__title">Your App Title</div>
+            </div>
+            <div className="header-panel header-panel--right">
+              <a className="header-item">Link 1</a>
+              <a className="header-item">Link 2</a>
+              <a className="header-item">Link 3</a>
+            </div>
+          </div>
+        </div>
       </header>
+      <div className="row dbl-padding-bottom">
+        <div className="col-lg-6 col-xl-4 base-padding-bottom">
+          <div className="subheader">Basic</div>
+          <nav className="sidebar" role="navigation">
+            <div className="sidebar__header">
+              <div className="sidebar__header-title">Title Area</div>
+            </div>
+            <ul>
+              <li className="sidebar__item">
+                <a tabIndex={0}>Home</a>
+              </li>
+              <li className="sidebar__item">
+                <a tabIndex={1}>Alerts</a>
+              </li>
+              <li className="sidebar__item">
+                <a tabIndex={2}>Charts</a>
+              </li>
+              <li className="sidebar__item">
+                <a tabIndex={3}>Devices</a>
+              </li>
+              <li className="sidebar__item">
+                <a tabIndex={4}>Settings</a>
+              </li>
+              <li className="sidebar__item">
+                <a tabIndex={5}>Help</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
