b6ae3c6c091494e0712f48ed3a583e2afdcf2181
Jay Young <jyoungta@cisco.com>
2022-05-19 12:25:17 -0400

    More x.509 extension parsing

--- src/CertExtension.tsx
+++ src/CertExtension.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { Label } from "@vkumov/react-cui-2.0";
+import * as forge from "node-forge";
 
 interface ICertificateExtensionBase {
   id: string;
@@ -21,6 +22,36 @@ function CertificateExtension(props: ICertificateExtension) {
           {...(props.extension as ICertificateExtensionKeyUsage)}
         ></CertificateExtensionKeyUsage>
       );
+    case "subjectAltName":
+      return (
+        <CertificateExtensionSubjectAltName
+          {...(props.extension as ICertificateExtensionSubjectAltName)}
+        ></CertificateExtensionSubjectAltName>
+      );
+    case "subjectKeyIdentifier":
+      return (
+        <CertificateExtensionSubjectKeyIdentifier
+          {...(props.extension as ICertificateExtensionSubjectKeyIdentifier)}
+        ></CertificateExtensionSubjectKeyIdentifier>
+      );
+    case "authorityKeyIdentifier":
+      return (
+        <CertificateExtensionAuthorityKeyIdentifier
+          {...props.extension}
+        ></CertificateExtensionAuthorityKeyIdentifier>
+      );
+    case "cRLDistributionPoints":
+      return (
+        <CertificateExtensionCrlDistributionPoints
+          {...props.extension}
+        ></CertificateExtensionCrlDistributionPoints>
+      );
+    case "authorityInfoAccess":
+      return (
+        <CertificateExtensionAuthorityInfoAccess
+          {...props.extension}
+        ></CertificateExtensionAuthorityInfoAccess>
+      );
     default:
       return (
         <>
@@ -32,7 +63,14 @@ function CertificateExtension(props: ICertificateExtension) {
 }
 
 interface ICertificateExtensionKeyUsage extends ICertificateExtensionBase {
-  foo: number;
+  cRLSign: boolean;
+  dataEncipherment: boolean;
+  decipherOnly: boolean;
+  digitalSignature: boolean;
+  encipherOnly: boolean;
+  keyAgreement: boolean;
+  keyCertSign: boolean;
+  nonRepudiation: boolean;
 }
 function CertificateExtensionKeyUsage(props: ICertificateExtensionKeyUsage) {
   return (
@@ -52,4 +90,156 @@ function CertificateExtensionKeyUsage(props: ICertificateExtensionKeyUsage) {
   );
 }
 
+interface ICertificateExtensionSubjectAltName
+  extends ICertificateExtensionBase {
+  altNames: { type: number; value: string }[];
+}
+function CertificateExtensionSubjectAltName(
+  props: ICertificateExtensionSubjectAltName
+) {
+  return (
+    <div>
+      <span>Subject Alternate Name:</span>
+      <ul>
+        {props.altNames.map((altName, index) => {
+          return (
+            <li key={index}>
+              {altName.type === 2 ? "DNS" : "?"} - {altName.value}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+interface ICertificateExtensionSubjectKeyIdentifier
+  extends ICertificateExtensionBase {
+  subjectKeyIdentifier: string;
+}
+function CertificateExtensionSubjectKeyIdentifier(
+  props: ICertificateExtensionSubjectKeyIdentifier
+) {
+  return (
+    <div>
+      <span>Subject Key Id: </span>
+      <span style={{ fontFamily: "monospace" }}>
+        {props.subjectKeyIdentifier.toUpperCase().match(/.{2}/g)?.join(":")}
+      </span>
+    </div>
+  );
+}
+
+interface IAiaASN1KeyId {
+  composed: boolean;
+  constructed: boolean;
+  tagClass: number;
+  type: number;
+  value: string;
+}
+
+interface IAsn1Object {
+  composed: boolean;
+  constructed: boolean;
+  tagClass: number;
+  type: number;
+  value: Array<IAsn1Object> | string;
+}
+
+function CertificateExtensionAuthorityKeyIdentifier(
+  props: ICertificateExtensionBase
+) {
+  let keyId = "";
+  const asn1Blob = forge.asn1.fromDer(props.value);
+  const asn1Object = asn1Blob.value[0] as IAiaASN1KeyId;
+  if (asn1Object.type === 0) {
+    const sha1Hash = asn1Object.value;
+    const hexString = forge.util.bytesToHex(sha1Hash).toUpperCase();
+    keyId = hexString.match(/.{2}/g)?.join(":") as string;
+  }
+  return (
+    <div>
+      Authority Key Identifier:{" "}
+      <span style={{ fontFamily: "monospace" }}>
+        {keyId ? `keyId=${keyId}` : "?"}
+      </span>{" "}
+    </div>
+  );
+}
+
+function CertificateExtensionCrlDistributionPoints(
+  props: ICertificateExtensionBase
+) {
+  const asn1Blob = forge.asn1.fromDer(props.value) as IAsn1Object;
+  const uriBlobs = (
+    ((asn1Blob.value as IAsn1Object[])[0].value as IAsn1Object[])[0]
+      .value as IAsn1Object[]
+  )[0].value as IAsn1Object[];
+  const uris = uriBlobs.map((uri) => uri.value);
+
+  return (
+    <div>
+      <p>CRL Distribution Points:</p>
+      <ul>
+        {uris.map((uri, index) => {
+          return (
+            <li key={index}>
+              <span style={{ fontFamily: "monospace" }}>
+                URI:{uri as string}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function CertificateExtensionAuthorityInfoAccess(
+  props: ICertificateExtensionBase
+) {
+  const a = new TextEncoder();
+  const asn1Blob = forge.asn1.fromDer(props.value) as IAsn1Object;
+  const accessMethods = (asn1Blob.value as IAsn1Object[]).map(
+    (accessMethod: IAsn1Object) => {
+      const oidArray = a.encode(
+        (accessMethod.value as IAsn1Object[])[0].value as string
+      );
+      const accessLocation = (accessMethod.value as IAsn1Object[])[1]
+        .value as string;
+      switch (oidArray[oidArray.length - 1]) {
+        case 1:
+          // OCSP
+          return (
+            <span>
+              OCSP:{" "}
+              <span style={{ fontFamily: "monospace" }}>{accessLocation}</span>
+            </span>
+          );
+        case 2:
+          return (
+            <span>
+              Ca Issuers:{" "}
+              <span style={{ fontFamily: "monospace" }}>{accessLocation}</span>
+            </span>
+          );
+
+        // CaIssuers
+        default:
+          // Unknown
+          throw Error("Unknown access method in AIA certificate extension");
+      }
+    }
+  );
+  return (
+    <>
+      <p>Authority Information Access:</p>
+      <ul>
+        {accessMethods.map((accessMethod, index) => (
+          <li key={index}>{accessMethod}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
 export default CertificateExtension;
